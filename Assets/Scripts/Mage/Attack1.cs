599f6a048cb49785d20175bd5b48af92669f2208
compagny912 <compagny912@gmail.com>
2015-08-06 17:50:21 +0200

    Update Attack1.cs
    Amélioration

--- Assets/Scripts/Mage/Attack1.cs
+++ Assets/Scripts/Mage/Attack1.cs
@@ -3,30 +3,16 @@ using System.Collections;
 
 public class Attack1 : StateMachineBehaviour {
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         animator.SetBool("onAttack1", true);
 	}
 
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		animator.SetBool ("Attack1", false);
 	}
 
-	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		animator.SetBool ("onAttack1", false);
         onAttack1.attackrealised = false;
 	}
-
-
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
 }
