cb7da921bf213b3255a386a512e54a75ce16126f
compagny912 <compagny912@gmail.com>
2015-08-06 17:57:49 +0200

    Update Attack2.cs
    **

--- Assets/Scripts/Mage/Attack2.cs
+++ Assets/Scripts/Mage/Attack2.cs
@@ -19,11 +19,6 @@ public class Attack2 : StateMachineBehaviour {
         onAttack2.attackrealised = false;
 	}
 
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
 	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
 	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
